c59b96d30c39b97d2dac4f0eb3bd212825364e97
Achmad Amin <achmadamin010@gmail.com>
2025-07-30 12:13:40 +0800

    Delete js directory

--- js/saran.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("saranForm").addEventListener("submit", function (e) {
-  const nama = document.getElementById("nama").value.trim();
-  const subjectField = document.getElementById("subjectField");
-
-  if (nama) {
-    subjectField.value = `Saran dari ${nama}`;
-  }
-});
